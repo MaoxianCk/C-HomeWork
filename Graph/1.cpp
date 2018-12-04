ff4404d4007d3d0018c2810dff8a3abd93095c7d
MaoxianCk <785315332@qq.com>
2018-12-04 23:30:03 +0800

    2018-12-04 23:29:58

---
+++ Graph/1.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+template <class DataType>
+class Graph
+{
+  private:
+    static const int MAXSIZE = 20;
+    DataType vertex[MAXSIZE];
+    bool arc[MAXSIZE][MAXSIZE];
+    int vertexNum, arcNum;
+    bool visited[MAXSIZE];
+
+    void DFSTraverse(int k);
+  public:
+    Graph();
+    ~Graph();
+    //k为遍历的起始顶点序号
+    void DFS(int k);
+    void BFS(int k);
+    void cleanVisited();
+};
+template <class DataType>
+Graph<DataType>::Graph()
+{
+    int n, e;
+    cout << "请输入图的顶点数：";
+    cin >> n;
+    //如果 > MAXSIZE 处理...
+    cout << "请输入图的边数：";
+    cin >> e; //如果 > MAXSIZE 处理...
+
+    this->vertexNum = n;
+    this->arcNum = e;
+    for (int i = 0; i < vertexNum; i++)
+    {
+        vertex[i] = i;
+        visited[i] = 0;
+    }
+
+    for (int i = 0; i < vertexNum; i++)
+    {
+        for (int j = 0; j < vertexNum; j++)
+        {
+            arc[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < arcNum; i++)
+    {
+        int a, b;
+        cout << "输入第" << i + 1 << "条边的两端顶点：";
+        cin >> a >> b;
+        arc[a][b] = 1;
+        arc[b][a] = 1;
+    }
+}
+
+template <class DataType>
+void Graph<DataType>::cleanVisited()
+{
+    for (int i = 0; i < vertexNum; i++)
+    {
+        visited[i] = 0;
+    }
+}
+template <class DataType>
+void Graph<DataType>::DFS(int k)
+{
+    cleanVisited();
+
+}
+template <class DataType>
+void Graph<DataType>::DFSTraverse(int k)
+{
+    cout << vertex[k];
+    visited[k] = 1;
+    for (int i = 0; i < vertexNum;i++)
+    {
+        if(arc[k][i]==1 && visited[i]==0)
+            {
+                DFSTraverse(i);
+            }
+    }
+}
+
+int main()
+{
+    return 0;
+}
--- OJ.cpp
+++ OJ.cpp
@@ -1,83 +1,32 @@
-#include <iomanip>
+#include <cmath>
 #include <iostream>
-#include <math.h>
-#include <memory.h>
 #include <string>
-
 using namespace std;
-
-double cal(double x, double y, int com)
+bool isChar(char c)
 {
-	switch (com)
-	{
-	case 0:
-		return x + y;
-	case 1:
-		return x - y;
-	case 2:
-		return x * y;
-	case 3:
-		return x / y;
-	}
-}
-bool f(double a, double b, double c)
-{
-	bool t = 0;
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			int p = 0;
-			if (fabs(cal(cal(a, b, i), c, j) - 18) < 0.0001)
-			{
-				t=1; p++;
-				//cout << i << " " << j <<" "<<p<< endl;
-				break;
-			}
-			if (fabs(cal(cal(a, c, i), b, j) - 18) < 0.0001)
-			{
-				t = 1;
-				p++;
-				//cout << i << " " << j <<" "<<p<< endl;
-				break;
-			}
-			if (fabs(cal(cal(b, a, i), c, j) - 18) < 0.0001)
-			{
-				t=1; p++;
-				//cout << i << " " << j <<" "<<p<< endl;
-				break;
-			}
-			if (fabs(cal(cal(b, c, i), a, j) - 18) < 0.0001)
-			{
-				t=1; p++;
-				//cout << i << " " << j <<" "<<p<< endl;
-				break;
-			}
-			if (fabs(cal(cal(c, b, i), a, j) - 18) < 0.0001)
-			{
-				t=1; p++;
-				//cout << i << " " << j <<" "<<p<< endl;
-				break;
-			}
-			if (fabs(cal(cal(c, a, i), b, j) - 18) < 0.0001)
-			{
-				t=1; p++;
-				//cout << i << " " << j <<" "<<p<< endl;
-				break;
-			}
-		}
-	}
-	return t;
+    if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9')
+        return true;
+    else
+        return false;
 }
 int main()
 {
-	int n;
-	cin >> n;
-	while (n--)
-	{
-		double a, b, c;
-		cin >> a >> b >> c;
-		cout << (f(a, b, c) ? "Yes" : "No") << endl;
-	}
-	return 0;
+    int t;
+    cin >> t;
+    string str;
+    getline(cin, str);
+    while (t--)
+    {
+        getline(cin, str);
+        int index;
+        for (index = str.length() - 1; index >= 0; index--)
+        {
+            if (isChar(str[index]))
+            {
+                break;
+            }
+        }
+        cout << (index != -1 ? str.substr(0, index + 1)+"\n" : "\n");
+    }
+    return 0;
 }
