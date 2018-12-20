f458acb0ef0896889872a3962e965f99024961bf
MaoxianCk <785315332@qq.com>
2018-12-20 11:33:05 +0800

    2018-12-20 11:33:03

--- FinalWar/4.cpp
+++ FinalWar/4.cpp
@@ -44,6 +44,12 @@ int inputNumber(string info, string errorInfo, int range)
     }
 }
 
+struct ArcVertexVal
+{
+    int vertex[2];
+    int value;
+};
+
 class Graph
 {
   private:
@@ -53,13 +59,15 @@ class Graph
     int vertex[MAXSIZE];
     int arc[MAXSIZE][MAXSIZE];
     bool visited[MAXSIZE];
+    ArcVertexVal arcs[MAXSIZE * (MAXSIZE - 1) / 2];
 
     void DFSTraverse(int k);
+    void dfsprint(string ans,ArcVertexVal e[], int root);
     void cleanVisited();
 
   public:
     Graph();
-    void topSort();
+    bool topSort();
     //k为遍历的起始顶点序号
     void DFS(int k);
     void BFS(int k);
@@ -105,6 +113,7 @@ Graph::Graph()
     }
 
     cout << "请输入有向图的边:" << endl;
+    int m = 0;
     for (int i = 0; i < arcNum; i++)
     {
         int a, b;
@@ -126,6 +135,9 @@ Graph::Graph()
         }
         cout << endl;
         arc[a][b] = value;
+        arcs[m].vertex[0] = a;
+        arcs[m].vertex[1] = b;
+        arcs[m++].value = value;
     }
 
     displayArc();
@@ -210,17 +222,58 @@ void Graph::BFS(int k)
     }
     cout << endl;
 }
-
+bool Graph::topSort()
+{
+    stack<int> s;
+    int vertexIn[MAXSIZE] = {0};
+    for (int i = 0; i < arcNum; i++)
+    {
+        vertexIn[arcs[i].vertex[1]]++;
+    }
+    for (int i = vertexNum - 1; i >= 0; i--)
+    {
+        if (vertexIn[i] == 0)
+        {
+            s.push(i);
+        }
+    }
+    int cnt = 0; //circle
+    while (!s.empty())
+    {
+        int v = s.top();
+        s.pop();
+        cout << " v" << vertex[v];
+        cnt++;
+        for (int i = vertexNum - 1; i >= 0; i--)
+        {
+            if (arc[v][i] != INT_MAX)
+            {
+                vertexIn[i]--;
+                if (vertexIn[i] == 0)
+                {
+                    s.push(i);
+                }
+            }
+        }
+    }
+    return (cnt == vertexNum ? true : false);
+}
 void Graph::criticalPath()
 {
+    if (!topSort())
+    {
+        cout << "该图有环无法计算关键路径！" << endl;
+        return;
+    }
+
     //最早发生时间
     int ve[MAXSIZE] = {0};
     //最晚发生时间
     int vl[MAXSIZE];
     //最早开始时间
-    int ee[MAXSIZE];
+    int ee[MAXSIZE * (MAXSIZE - 1) / 2];
     //最晚开始时间
-    int el[MAXSIZE];
+    int el[MAXSIZE * (MAXSIZE - 1) / 2];
 
     ve[0] = 0;
     for (int i = 0; i < vertexNum; i++)
@@ -238,25 +291,101 @@ void Graph::criticalPath()
     vl[vertexNum - 1] = ve[vertexNum - 1];
     for (int i = vertexNum - 1; i >= 0; i--)
     {
-        int min=INT_MAX;
+        int min = INT_MAX;
         for (int j = vertexNum - 1; j >= 0; j--)
         {
 
-            if (arc[i][j] !=INT_MAX &&  vl[j]-arc[i][j] < min)
+            if (arc[i][j] != INT_MAX && vl[j] - arc[i][j] < min)
             {
                 min = vl[j] - arc[i][j];
                 vl[i] = min;
             }
         }
     }
-    for (int i = 0; i < vertexNum;i++)
+
+    for (int i = 0; i < arcNum; i++)
     {
-        cout << ve[i] << "  ";
+        ee[i] = ve[arcs[i].vertex[0]];
+        el[i] = vl[arcs[i].vertex[1]] - arcs[i].value;
     }
-    cout << endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED);
+
     for (int i = 0; i < vertexNum; i++)
     {
-        cout << vl[i] << "  ";
+        cout << "\tv" << i + 1;
+    }
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN);
+
+    cout << endl;
+    cout << "ve:\t";
+    for (int i = 0; i < vertexNum; i++)
+    {
+        cout << ve[i] << "\t";
+    }
+    cout << endl;
+    cout << "vl:\t";
+    for (int i = 0; i < vertexNum; i++)
+    {
+        cout << vl[i] << "\t";
+    }
+    cout << endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED);
+
+    for (int i = 0; i < arcNum; i++)
+    {
+        cout << "\ta" << i + 1;
+    }
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN);
+
+    cout << endl;
+    cout << "ee:\t";
+    for (int i = 0; i < arcNum; i++)
+    {
+        cout << ee[i] << "\t";
+    }
+    cout << endl;
+    cout << "el:\t";
+    for (int i = 0; i < arcNum; i++)
+    {
+        cout << el[i] << "\t";
+    }
+
+    cout << endl;
+    cout << "关键路径:";
+    string ans[MAXSIZE];
+    for (int i = 0; i < MAXSIZE; i++)
+    {
+        ans[i] = "";
+    }
+    ArcVertexVal e[MAXSIZE * (MAXSIZE - 1) / 2];
+    int m = 0;
+    for (int i = 0; i < arcNum; i++)
+    {
+        if (el[i] - ee[i] == 0)
+        {
+            e[m].vertex[0] = arcs[i].vertex[0];
+            e[m].vertex[1] = arcs[i].vertex[1];
+            e[m++].value = arcs[i].value;
+            cout << e[i].vertex[0] << "  " << e[i].vertex[1] << endl;
+        }
+    }
+    //dfsprint(("v" + to_string(e[0].vertex[0])), e, e[0].vertex[0]);
+}
+void Graph::dfsprint(string ans, ArcVertexVal e[], int root)
+{
+    bool flag = false;
+    for (int i = 0; i < arcNum; i++)
+    {
+        if (e[i].vertex[0] == root)
+        {
+            flag = true;
+            ans = ans + " -> v" + to_string(root + 1);
+            dfsprint(ans, e, e[i].vertex[1]);
+        }
+    }
+    if(!flag)
+    {
+        cout << ans << endl;
     }
 }
 int main()
